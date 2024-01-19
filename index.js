b38a534149e3663e56b40027ea477567a7ee4acd
Victor Vasquez <87541724+VictorDVP@users.noreply.github.com>
2024-01-19 13:47:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const authenticatedUser = (username,password)=>{
 
 const app = express();
 
-app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+app.use(session({secret:"fingerpint",resave:false,saveUninitialized:false}));
 
 app.use(express.json());
 
